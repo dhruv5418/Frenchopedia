652df8a7dcc01eb6675aa5c49efe6000635873b1
Dhruv <dhruvj5418@gmail.com>
2020-05-14 13:35:44 -0400

    Select level with firestore collection update java code added.

--- app/src/main/java/com/example/frenchopedia/SelectlevelFragment.java
+++ app/src/main/java/com/example/frenchopedia/SelectlevelFragment.java
@@ -2,57 +2,53 @@ package com.example.frenchopedia;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SelectlevelFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SelectlevelFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button btn_next;
+    RadioGroup selectLvl;
+    RadioButton radio_button;
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+    private FirebaseUser curUser;
+    NavController navController;
 
     public SelectlevelFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SelectlevelFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SelectlevelFragment newInstance(String param1, String param2) {
-        SelectlevelFragment fragment = new SelectlevelFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        auth=FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
+        curUser=auth.getCurrentUser();
+
     }
 
     @Override
@@ -60,5 +56,42 @@ public class SelectlevelFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_selectlevel, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_next=view.findViewById(R.id.btn_lvl);
+        selectLvl=view.findViewById(R.id.selectLevel);
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = selectLvl.getCheckedRadioButtonId();
+                int set;
+                radio_button=view.findViewById(selectedId);
+                Map<String,Object> usermap=new HashMap<>();
+                if(selectedId==R.id.lvl_1){
+                    set=1;
+                }else if(selectedId==R.id.lvl_2){
+                    set=2;
+                }else{
+                    set=3;
+                }
+                usermap.put("Level",set);
+                db.collection("Users").document(curUser.getUid()).update(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getActivity().getApplicationContext(),"Level selected successfully",Toast.LENGTH_LONG);
+                            navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                            auth.signOut();
+                            navController.navigate(R.id.loginfragment);
+                        }else{
+                            Log.d("Select Level Fragment","onFailure: Level Selection"+task.getException().getMessage());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
