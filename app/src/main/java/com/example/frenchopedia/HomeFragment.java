584e494aa77a5870a67dda039bc8a4527e155ebf
Dhruv <dhruvj5418@gmail.com>
2020-05-17 18:12:12 -0400

    Tool bar added with all menu items

--- app/src/main/java/com/example/frenchopedia/HomeFragment.java
+++ app/src/main/java/com/example/frenchopedia/HomeFragment.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -20,12 +24,12 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
 
-    String data="<div id=\"widget\" style=\"width:460px;height:190px;margin:0 auto;\"><iframe src=\"https://www.innovativelanguage.com/widgets/wotd/embed.php?language=French&type=small&bg=url%28/widgets/wotd/skin/images/small/Cherry_Blossom.png%29%20no-repeat%200%200&content=%23000&header=%23DBB9D4&highlight=%23D973BE&opacity=.15&scrollbg=%23D991C6&sound=%23D973BE&text=%23BF43A5&quiz=N\" width=\"160\" height=\"190\" frameborder=\"0\" scrolling=\"no\"></iframe><div style=\"font:bold 9px/16px Verdana; padding:0; height:16px;\"><div style=\"float:left; margin:0;\"><a href=\"https://www.frenchpod101.com/french-phrases/\" target=\"_parent\" title=\"Get Widget\" style=\"font-family: Helvetica, Arial, sans-serif;font-size: 11px;color: #00ACED;\" rel=\"nofollow\">Get Widget</a></div><div style=\"float:right; margin:0;\"><a href=\"https://www.frenchpod101.com\" target=\"_parent\" title=\"Learn French\" style=\"font-family: Helvetica, Arial, sans-serif;font-size: 11px;color: #00ACED;\" rel=\"nofollow\">Learn French</a></div></div></div>";
+    String data="<div id=\"widget\";\"><iframe src=\"https://www.innovativelanguage.com/widgets/wotd/embed.php?language=French&type=small&bg=url%28/widgets/wotd/skin/images/small/Cherry_Blossom.png%29%20no-repeat%200%200&content=%23000&header=%23DBB9D4&highlight=%23D973BE&opacity=.15&scrollbg=%23D991C6&sound=%23D973BE&text=%23BF43A5&quiz=N\" width=\"360\" height=\"190\" frameborder=\"0\" scrolling=\"no\"></iframe></div>";
     WebView webView;
-
     private FirebaseAuth auth;
     private FirebaseUser curUser;
     NavController navController;
+    Toolbar toolbar;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -34,20 +38,25 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         auth=FirebaseAuth.getInstance();
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         webView=view.findViewById(R.id.webView);
         Button b=view.findViewById(R.id.logout);
+        toolbar=view.findViewById(R.id.toolbar_home);
+        toolbar.inflateMenu(R.menu.tool_home);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadData(data , "text/html" , null);
 
