5cfc27be57695621301c45b7b3500ac3feca7b5d
Dhruv <dhruvj5418@gmail.com>
2020-06-28 00:17:00 -0400

    Email id update done.

--- app/src/main/java/com/example/frenchopedia/SettingsActivity.java
+++ app/src/main/java/com/example/frenchopedia/SettingsActivity.java
@@ -4,29 +4,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.Navigation;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
@@ -38,18 +54,23 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth auth;
     private FirebaseUser curUser;
     CircleImageView imageView;
-    EditText edt_fName,edt_lName,edt_email;
+    EditText edt_fName,edt_lName;
     Toolbar toolbar;
+    String nEmail;
+    Button btn_updateEmail,btn_signOut,btn_resetPass,btn_feedBack;
     private FirebaseFirestore db;
-    TextView txt_level;
+    TextView txt_level,txt_email;
+    String fName,lName,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,8 +93,11 @@ public class SettingsActivity extends AppCompatActivity {
         imageView=findViewById(R.id.img_p);
         edt_fName=findViewById(R.id.txt_fName);
         edt_lName=findViewById(R.id.txt_lName);
-        edt_email=findViewById(R.id.txt_email);
+        txt_email=findViewById(R.id.txt_email);
         txt_level=findViewById(R.id.lbl_level);
+        btn_updateEmail=findViewById(R.id.update_email);
+        btn_updateEmail.setOnClickListener(this);
+       // btn_signOut=findViewById(R.id.)
         imageView.setOnClickListener(changeProfile);
         loadData();
     }
@@ -91,7 +115,7 @@ public class SettingsActivity extends AppCompatActivity {
            public void onClickback(DocumentSnapshot documentSnapshot) {
                edt_fName.setText((CharSequence) documentSnapshot.get("First Name"));
                edt_lName.setText((CharSequence) documentSnapshot.get("Last Name"));
-               edt_email.setText((CharSequence) documentSnapshot.get("Email"));
+               txt_email.setText((CharSequence) documentSnapshot.get("Email"));
              int a=(int)(long) documentSnapshot.get("Level");
                switch (a){
                    case 1: txt_level.setText("Level:- "+getString(R.string.lvl_1));
@@ -114,6 +138,110 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.update_email: updateEmail(v);
+                    break;
+        }
+
+    }
+
+    private void updateEmail(View v) {
+        View popupview=getLayoutInflater().inflate(R.layout.popupview,null);
+        final PopupWindow popupWindow=new PopupWindow(popupview, ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT,true);
+        if(Build.VERSION.SDK_INT>=21){
+            popupWindow.setElevation(5.0f);
+        }
+        final EditText edtPEmail=popupview.findViewById(R.id.edt_popemail);
+        final EditText edtPPass=popupview.findViewById(R.id.edt_poppassword);
+        final EditText edtNEmail=popupview.findViewById(R.id.edt_newEmail);
+
+        //Log.d("Setting","email="+nEmail);
+        Button btnSubmit=popupview.findViewById(R.id.btn_poplogin);
+        popupWindow.setFocusable(true);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.orenge_button)));
+        popupWindow.showAtLocation(v, Gravity.CENTER,0,0);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(edtPEmail.getText().toString())){
+                    edtPEmail.setError("Email cannot be blank!");
+                    edtPEmail.requestFocus();
+                }else if(TextUtils.isEmpty(edtPPass.getText().toString())) {
+                    edtPPass.setError("Password cannot be blank!");
+                    edtPPass.requestFocus();
+                }else if(TextUtils.isEmpty(edtNEmail.getText().toString())) {
+                    edtPPass.setError("New Email cannot be blank!");
+                    edtPPass.requestFocus();
+                }else{
+                    if(edtPPass.getText().toString().length()<6){
+                        edtPPass.setError("Invalid password,Should be at least 6 characters");
+                        edtPPass.requestFocus();
+                    } else {
+                        AuthCredential credential= EmailAuthProvider.getCredential(edtPEmail.getText().toString(),edtPPass.getText().toString());
+                        curUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    nEmail=edtNEmail.getText().toString();
+                                    Log.d("Setting","email="+nEmail);
+                                    curUser.updateEmail(nEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Log.d("Exception", "log Succ: ");
+                                                curUser=auth.getCurrentUser();
+                                                Map<String,Object> usermap=new HashMap<>();
+                                                usermap.put("Email",nEmail);
+                                                //int a=0;
+                                                //usermap.put("Level",a);
+                                                db.collection("Users").document(curUser.getUid()).update(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if(task.isSuccessful()){
+                                                            Toast.makeText(getApplicationContext(),"Email Updated Successfully!",Toast.LENGTH_LONG).show();
+                                                            popupWindow.dismiss();
+                                                            loadData();
+                                                            //Toast.makeText(getActivity().getApplicationContext(),"Please verify your Email address!",Toast.LENGTH_LONG).show();
+                                                        }
+                                                    }
+                                                });
+                                            }else{
+                                                try{
+                                                throw task.getException();
+                                            } catch (FirebaseAuthUserCollisionException already) {
+                                                Toast.makeText(getApplicationContext(),"Email Already Exist!",Toast.LENGTH_LONG).show();
+                                                edtNEmail.getText().clear();
+                                                edtNEmail.requestFocus();
+                                                edtNEmail.setError("Enter Other Emil");
+                                                edtPEmail.getText().clear();
+                                                edtPPass.getText().clear();
+
+                                            }
+                                            catch (Exception e){
+                                                Log.d("Exception", "onComplete: " + e.getMessage());
+                                                Toast.makeText(getApplicationContext(),"Register Failed!",Toast.LENGTH_LONG).show();
+                                            }
+
+                                            }
+                                        }
+                                    });
+                                }else {
+                                    Log.d("Setting Activity","onFailure: Authentication"+task.getException().getMessage());
+                                    edtPEmail.getText().clear();
+                                    edtPPass.getText().clear();
+                                    edtPEmail.setError("Please enter correct details");
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
     }
 
     private interface FirestoreCallback{
@@ -235,11 +363,59 @@ public class SettingsActivity extends AppCompatActivity {
         public boolean onMenuItemClick(MenuItem item) {
             switch (item.getItemId()){
                 case R.id.done:
-                    Toast.makeText(getApplicationContext(),"DOne",Toast.LENGTH_LONG).show();
+                                fName=edt_fName.getText().toString();
+                                lName=edt_lName.getText().toString();
+                                //email=edt_email.getText().toString();
+
+                             if(!checkEmptyField()){
+                                updateData();
+                             }
+
+                    Toast.makeText(getApplicationContext(),"Done",Toast.LENGTH_LONG).show();
             }
 
             return true;
         }
     };
+    public  boolean checkEmptyField(){
+       if(TextUtils.isEmpty(fName)){
+            edt_fName.setError("Name cannot be blank!");
+            edt_fName.requestFocus();
+            return true;
+        }else if(TextUtils.isEmpty(lName)){
+            edt_lName.setError("Name cannot be blank!");
+            edt_lName.requestFocus();
+            return true;
+        } /*else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edt_email.setError("Wrong Email Format!");
+            edt_email.getText().clear();
+            edt_email.requestFocus();
+            return true;
+        }*/
+        else{
+            return false;
+        }
+    }
+
+    public void updateData(){
+        curUser=auth.getCurrentUser();
+        Map<String,Object> usermap=new HashMap<>();
+        usermap.put("First Name",fName);
+        usermap.put("Last Name",lName);
+        //usermap.put("Email",email);
+        //int a=0;
+        //usermap.put("Level",a);
+        db.collection("Users").document(curUser.getUid()).update(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Updated Successfully!",Toast.LENGTH_LONG).show();
+                    loadData();
+                    //Toast.makeText(getActivity().getApplicationContext(),"Please verify your Email address!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 
 }
