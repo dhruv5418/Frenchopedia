48da8458eb3fdca7bbbe1be7f7ac4d900db4ce6c
gakha <gakhartanvi@gmail.com>
2020-08-02 22:40:49 -0400

    Video added in course fragment.

--- app/src/main/java/com/example/frenchopedia/CourseFragment.java
+++ app/src/main/java/com/example/frenchopedia/CourseFragment.java
@@ -1,19 +1,29 @@
 package com.example.frenchopedia;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 
+public class CourseFragment extends Fragment implements View.OnClickListener {
 
-public class CourseFragment extends Fragment {
+    LinearLayout l1;
+
+
+    NavController navController;
 
 Toolbar toolbar;
 
@@ -25,7 +35,8 @@ Toolbar toolbar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+
+
     }
 
     @Override
@@ -38,7 +49,25 @@ Toolbar toolbar;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         toolbar=view.findViewById(R.id.toolbar_course);
+        l1=view.findViewById(R.id.video1);
+        l1.setOnClickListener(this);
         toolbar.inflateMenu(R.menu.tool_course);
+        navController= Navigation.findNavController(getActivity(),R.id.nav_dashboard);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        Bundle b=new Bundle();
+        switch (v.getId()){
+            case R.id.video1:
+                b.putInt("index",1);
+                    navController.navigate(R.id.playvideoFragment,b);
+        }
+
+    }
+
+
 }
