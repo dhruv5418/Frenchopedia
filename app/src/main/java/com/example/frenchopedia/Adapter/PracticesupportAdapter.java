75aff9e0baada47575ff777f1b52dc6a46202773
Kamal <saikamaljit181294@gmail.com>
2020-08-04 18:24:17 -0400

    Json data retrieved from firestore database and shown in recycler view for day names practice.

---
+++ app/src/main/java/com/example/frenchopedia/Adapter/PracticesupportAdapter.java
@@ -0,0 +1,66 @@
+package com.example.frenchopedia.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.frenchopedia.Model.Material;
+import com.example.frenchopedia.Model.Practice_;
+import com.example.frenchopedia.R;
+
+import java.util.ArrayList;
+
+public class PracticesupportAdapter extends RecyclerView.Adapter<PracticesupportAdapter.ViewHolder> {
+    private ArrayList<Material> arrayListPractice;
+    private Context context;
+    private View.OnClickListener clickListener;
+
+    public PracticesupportAdapter(ArrayList<Material> arrayListPractice, Context context) {
+        this.arrayListPractice = arrayListPractice;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Glide.with(context).asBitmap().load(arrayListPractice.get(position).getImage()).into(holder.img_view);
+        holder.txt_view.setText(arrayListPractice.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayListPractice.size();
+    }
+
+    public void setOnClickListner(View.OnClickListener onClickListner) {
+        clickListener = onClickListner;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView img_view;
+        TextView txt_view;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img_view = itemView.findViewById(R.id.imgv_pokemon);
+            txt_view = itemView.findViewById(R.id.txt_pokemon);
+            itemView.setTag(this);
+            itemView.setOnClickListener(clickListener);
+        }
+    }
+}
--- app/src/main/java/com/example/frenchopedia/PracticesupportActivity.java
+++ app/src/main/java/com/example/frenchopedia/PracticesupportActivity.java
@@ -1,14 +1,84 @@
 package com.example.frenchopedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.frenchopedia.Adapter.PracticesupportAdapter;
+import com.example.frenchopedia.Model.Material;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class PracticesupportActivity extends AppCompatActivity {
+    String value;
+    PracticesupportAdapter practiceAdapter;
+    DatabaseReference d;
+    ArrayList<Material> days = new ArrayList<>();
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practicesupport);
+        Intent intent = getIntent();
+        value = intent.getStringExtra("Title");
+        select(value);
     }
+
+    private void select(String value) {
+            switch (value) {
+                case "Days":
+                    d = FirebaseDatabase.getInstance().getReference().child("Days");
+                    Log.d("MainFragment", "idmon=" + d);
+                    loadJson();
+                    break;
+            }
+    }
+
+
+    private void loadJson() {
+        Log.d("Json","called="+d);
+        //Toast.makeText(getApplicationContext(),"Load Json"+d,Toast.LENGTH_LONG).show();
+        d.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(getApplicationContext(),"Not Exist"+dataSnapshot,Toast.LENGTH_LONG).show();
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(getApplicationContext(),"Exist"+dataSnapshot,Toast.LENGTH_LONG).show();
+                    for (DataSnapshot npsnapshot : dataSnapshot.getChildren()) {
+                        Material l = npsnapshot.getValue(Material.class);
+                        days.add(l);
+                        Toast.makeText(getApplicationContext(),"Converted"+days,Toast.LENGTH_LONG).show();
+                    }
+                    generateView(days);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),""+databaseError,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void generateView(ArrayList<Material> materials) {
+        practiceAdapter = new PracticesupportAdapter(materials, getApplicationContext());
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView=findViewById(R.id.recyclerView_PracticeS);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(practiceAdapter);
+        Toast.makeText(getApplicationContext(),"done",Toast.LENGTH_LONG).show();
+        //practiceAdapter.setOnClickListner(onClickListener);
+    }
+
 }
